6741ca899266113e2180da145c26e63a9427965b
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-01 09:25:49 -0500

    feat: :ambulance: De html a php
    El formulario que estaba en html se le paso al php asi formando un frankestain para ver el resultado ahi mismo, se solucionó el error de que lea 0 como numero, pero esta en proceso para validar la división entre 0

--- api.php
+++ api.php
@@ -1,42 +1,76 @@
-<?php
+<!DOCTYPE html>
+<html lang="sp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EnvioData</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="api.php" method="post">
+        <input type="text" pattern="[0-9-.-,]+" name="num1">
+        <select name="operación" id="">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+            <option value="%">%</option>
+        </select>
+        <input type="text" pattern="[0-9-.-,]+" name="num2">
+        <input type="submit" value="=">
+    </form>
+    <?php
     
     /* echo "Datos en get\n";
     print_r($_GET);
     echo "Datos en post\n";
     print_r($_POST);
  */
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $operador = $_POST['operación'];
 
     
-    switch ($operador) {
-        case '+':
-            $solucion = $num1 + $num2 ;
-            $todo = "de la suma es {$solucion}";
-            break;
-        case '-':
-            $solucion = $num1 - $num2 ;
-            $todo = "de la resta es {$solucion}";
-            break;
-        case '*':
-            $solucion = $num1 * $num2 ;
-            $todo = "de la multiplicación es {$solucion}";
-            break;
-        case '/':
-            $solucion = $num1 / $num2 ;
-            $todo = "de la división es {$solucion}";
-            break; 
-        case '%':
-            $solucion = $num1 % $num2 ;
-            $todo = "de la operación modular es {$solucion}";
-            break;       
-        default:
-            
-            break;
-    }
 
-    echo "El resultado {$todo}"
+    $num1 = intval($_POST['num1']);
+    $num2 = intval($_POST['num2']);
+    $operador = $_POST['operación'];
+
+    if(!is_numeric($num1) && !is_numeric($num2)){
+        echo "Agrega los números";
+    }elseif(!is_numeric($num1)){
+        echo "Agrega el primer número";
+    }elseif(!is_numeric($num2)){
+        echo "Agrega el segundo número";
+    }else{
+        switch ($operador) {
+            case '+':
+                $solucion = $num1 + $num2 ;
+                $todo = "de la suma es {$solucion}";
+                break;
+            case '-':
+                $solucion = $num1 - $num2 ;
+                $todo = "de la resta es {$solucion}";
+                break;
+            case '*':
+                $solucion = $num1 * $num2 ;
+                $todo = "de la multiplicación es {$solucion}";
+                break;
+            case '/':
+                $solucion = $num1 / $num2 ;
+                $todo = "de la división es {$solucion}";
+                break; 
+            case '%':
+                $solucion = $num1 % $num2 ;
+                $todo = "de la operación modular es {$solucion}";
+                break;       
+            default:
+                
+                break;
+        }
+    
+        echo "El resultado {$todo}";
+    };
+    
+    
 
     /* switch ($operador) {
         case '+':
@@ -66,3 +100,7 @@
 
     echo "El resultado {$todo}" */
 ?>
+</body>
+</html>
+
+
