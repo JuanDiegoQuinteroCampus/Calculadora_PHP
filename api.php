1dbee02ccca1f4ee2e05c7568c5104015823683e
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-01 21:35:58 -0500

    feat: :fire: Remove files
    Se eliminaron unas filas que contenian un algoritmo de respaldo comentado, pero ya funciona correctamente por lo tanto se pudieron eliminar

--- api.php
+++ api.php
@@ -28,8 +28,6 @@
     print_r($_POST);
  */
 
-    
-
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
     $operacion = $_POST['operación'];
@@ -74,36 +72,7 @@
     
         echo "El resultado {$todo}";
     };
-    
-    
 
-    /* switch ($operador) {
-        case '+':
-            $solucion = $num1 + $num2 ;
-            $todo = "de la suma es {$solucion}";
-            break;
-        case '-':
-            $solucion = $num1 - $num2 ;
-            $todo = "de la resta es {$solucion}";
-            break;
-        case '*':
-            $solucion = $num1 * $num2 ;
-            $todo = "de la multiplicación es {$solucion}";
-            break;
-        case '/':
-            $solucion = $num1 / $num2 ;
-            $todo = "de la división es {$solucion}";
-            break; 
-        case '%':
-            $solucion = $num1 % $num2 ;
-            $todo = "de la operación modular es {$solucion}";
-            break;       
-        default:
-            
-            break;
-    }
-
-    echo "El resultado {$todo}" */
 ?>
 </body>
 </html>
